3bfb86258072704d0ea3fb68e864edb90a687e3b
Macia Estela <maciaestela97@gmail.com>
2022-07-27 23:33:20 +0200

    implement wait to element

--- js/index.js
+++ js/index.js
@@ -15,3 +15,23 @@ async function getPost(id) {
         console.log(error);
     }
 }
+
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
